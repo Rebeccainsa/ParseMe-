db9fb0f22c3e77a0cd66880b8f870136cbcbd79c
Rebeccainsa <rebeccavo98@gmail.com>
2023-10-16 16:39:02 +0200

    parse_line done

--- parseme.py
+++ parseme.py
@@ -1,1 +1,22 @@
 # Parseme.py
+#from PIL import Image
+
+# creating a object
+#im = Image.open("https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpeg")
+
+#im.show()
+
+x=["cutty","https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg", "gray/white", "3"]
+
+def parse_line(): 
+    print ("Nom:", x[0])
+    print ("Color:", x[2])
+    print ("Age: ", x[3])
+
+parse_line()
+
+
+def parse_file(): 
+    fhand = open('cats.txt')
+    parse_line(fhand)
+    

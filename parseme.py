0550f830a063f8b1399b1a8fb36e49cd0d76f359
Rebeccainsa <rebeccavo98@gmail.com>
2023-10-17 09:12:12 +0200

    Add minor fixes in parse_line

--- parseme.py
+++ parseme.py
@@ -1,27 +1,43 @@
 # Parseme.py
-#from PIL import Image
 
-# creating a object
-#im = Image.open("https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpeg")
+test_line = "cutty;https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg;gray/white;3"
+# We want to split test_line like this:
+# ["cutty","https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg", "gray/white", "3"]
 
-#im.show()
+def parse_line(line): 
+    #TODO
 
-import re #contient la fonction split pour la fonction parse_line 
+    print ("Nom:", line[0])
+    print ("Color:", line[2])
+    print ("Age: ", line[3])
 
-x=["cutty","https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg", "gray/white", "3"]
+parse_line(test_line)
 
-def parse_line(): 
-    print ("Nom:", x[0])
-    print ("Color:", x[2])
-    print ("Age: ", x[3])
 
-parse_line()
 
+""" TODO
 
 fhand = open('cats.txt')
-list=re.split("\n", fhand)
+list = split("\n", fhand)
 
-#def parse_file(): 
-    #for i in len(list):
-       # print ("Nom:", list[i,0])
-#parse_file()
+def parse_file(): 
+    for i in len(list):
+       print ("Nom:", list[i,0])
+parse_file()
+
+"""
+
+
+###############################################
+
+# i = 0
+
+# def incr(i):
+#     i += 1
+#     print(i)    # 1
+#     return i
+
+# print(i)        # 0
+# j = incr(i)
+# print(j)        # 1
+# print(i)        # 0
